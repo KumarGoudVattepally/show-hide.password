bee375429c87171c26d49b2aa464db6f08026faf
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-26 13:56:19 +0530

    sh

---
+++ index.js
@@ -0,0 +1,35 @@
+// function isfocus(event) {
+//     let target = event.target;
+//     target.style.backgroundColor = "red";
+//   }
+//   function isblur(event) {
+//     let target = event.target;
+//     target.style.backgroundColor = "white";
+//   }
+//   function isover(event) {
+//     let target = event.target;
+//     target.style.backgroundColor = "blue";
+//   }
+//   function isdown(event) {
+//     let target = event.target;
+//     target.style.backgroundColor = "orange";
+//   }
+//   function iskey(event) {
+//     let target = event.target;
+//     target.style.backgroundColor = "grey";
+//   }
+//   function iskeyup(event) {
+//     let target = event.target;
+//     target.style.backgroundColor = "grey";
+//     para.innerHTML = "Hello Kumar";
+//   }
+
+
+//  let passwo=document.getElementById("pass")
+// function clk(){
+//     if(passwo.type=="password"){
+//         passwo.type="text"
+//     }else{
+//         passwo.type="password"
+//     }
+// }
